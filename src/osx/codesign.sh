3892d5af18f815cd4170336eca0af2f6e3a0d179
ta <ta@zzv.io>
2021-06-28 20:43:32 +0500

    Fixed mac

--- src/osx/codesign.sh
+++ src/osx/codesign.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sw_vers
-cp target/client/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/
+mkdir -p src/osx/zzv.app/Contents/MacOS/
+cp target/client/x86_64-darwin/Zzv-view src/osx/zzv.app/Contents/MacOS/Zzv-view
 cd src/osx
 sed -e 's/CFBundleVersionPlaceHolder/'$GITHUB_RUN_NUMBER'/g' zzv.app/Contents/Info.plist > zzv.app/Contents/Temp.plist
 mv zzv.app/Contents/Temp.plist zzv.app/Contents/Info.plist
